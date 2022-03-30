65ab392033773c0f3e711d29ee00623933bf9a20
pierthodo <pierthodo@gmail.com>
2022-03-30 14:09:27 +0100

    Update run_exp.sh

--- script/run_exp.sh
+++ script/run_exp.sh
@@ -4,7 +4,9 @@ for env in Hopper-v2 Pendulum-v0 Humanoid-v2 HalfCheetah-v2 FetchPickAndPlace-v1
 do
   for algo in PPO SAC ARS
   do
+    echo "Running $algo $env"
     rllib train --run $algo --env $env --config '{"framework": "torch"}' --checkpoint-freq 100 &
+    sleep 30
   done
 done
 wait 
